b8c8899579543a51e5eb03673830b5a314472a3e
catlookatyou <woozoo6834@gmail.com>
2020-04-22 17:46:53 +0800

    jwt

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,20 +5,10 @@ use App\Http\Controllers\Controller;
 use App\User; 
 use Illuminate\Support\Facades\Auth; 
 use Validator;
+
 class UserController extends Controller 
 {
-public $successStatus = 200;
-
-    public function login(){ 
-        if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){ 
-            $user = Auth::user(); 
-            $success['token'] =  $user->createToken('MyApp')-> accessToken; 
-            return response()->json(['success' => $success], $this-> successStatus); 
-        } 
-        else{ 
-            return response()->json(['error'=>'Unauthorised'], 401); 
-        } 
-    }
+    public $successStatus = 200;
 
     public function register(Request $request) 
     { 
@@ -28,20 +18,44 @@ public $successStatus = 200;
             'password' => 'required', 
             'c_password' => 'required|same:password', 
         ]);
-if ($validator->fails()) { 
+        if ($validator->fails()) { 
             return response()->json(['error'=>$validator->errors()], 401);            
         }
-$input = $request->all(); 
+        $input = $request->all(); 
         $input['password'] = bcrypt($input['password']); 
         $user = User::create($input); 
-        $success['token'] =  $user->createToken('MyApp')-> accessToken; 
+        $success['token'] =  $user->createToken('MyApp')->accessToken; 
         $success['name'] =  $user->name;
-return response()->json(['success'=>$success], $this-> successStatus); 
+        return response()->json(['success'=>$success], $this->successStatus); 
     }
 
-    public function details() 
+    public function login(){ 
+        if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){ 
+            $user = Auth::user(); 
+            //Auth::logout();
+            $success['token'] =  $user->createToken('MyApp')->accessToken; 
+            return response()->json(['success' => $success], $this->successStatus); 
+        } 
+        else{ 
+            return response()->json(['error'=>'Unauthorised'], 401); 
+        } 
+    }
+
+    public function logout(Request $request)
+    {
+        //Auth::guard('web')->logout();
+        $token = $request->user()->token();
+        $token->revoke();
+        $token->delete();
+        $user = Auth::user();
+        return response()->json(['success' => $user], $this->successStatus); 
+    }
+
+    public function details(Request $request) 
     { 
-        $user = Auth::user(); 
-        return response()->json(['success' => $user], $this-> successStatus); 
+        $user = Auth::user();
+        $token = $request->user()->token();
+        $bearerToken = $request->bearerToken(); 
+        return response()->json(['success' => $user, 'bearerToken' => $bearerToken, 'token' => $token], $this-> successStatus); 
     } 
 }
--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 Route::post('login', 'UserController@login');
 Route::post('register', 'UserController@register');
-Route::group(['middleware' => 'auth:api'], function()
-{
-   Route::post('details', 'UserController@details');
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::post('logout', 'UserController@logout');
+    Route::post('details', 'UserController@details');
 });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
